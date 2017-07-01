7022abc19bcec0405e196ef450114d3338323b68
Tim <tim.huang61@gmail.com>
2017-07-01 17:32:21 +0800

    modify update and create test func for service

--- GOOS_SampleTests/Models/BudgetServiceTest.cs
+++ GOOS_SampleTests/Models/BudgetServiceTest.cs
@@ -12,17 +12,35 @@ namespace GOOS_SampleTests.Models
     public class BudgetServiceTest
     {
         private BudgetService _budgetService;
-        private IRespository<Budget> _budgetRespositoryStub = Substitute.For<IRespository<Budget>>();
+        private IRespository<Budget> _budgetRepositoryStub = Substitute.For<IRespository<Budget>>();
 
         [TestMethod]
         public void CreateTest_should_invoke_repository_one_time()
         {
-            _budgetService = new BudgetService(_budgetRespositoryStub);
-            var budgetFromDb = new Budget { Amount = 999, YearMonth = "2017-02" };
-            _budgetRespositoryStub.Read(Arg.Any<Func<Budget, bool>>()).ReturnsForAnyArgs(budgetFromDb);
+            // test event 的結果
+            _budgetService = new BudgetService(_budgetRepositoryStub);
+            var wasCreated = false;
+            _budgetService.Created += (sender, e) => wasCreated = true;
             var model = new BudgetAddViewModel { Amount = 2000, Month = "2017-02" };
             _budgetService.Create(model);
-            _budgetRespositoryStub.Received().Save(Arg.Is<Budget>(x => x.Amount == 2000 && x.YearMonth == "2017-02"));
+            _budgetRepositoryStub.Received().Save(Arg.Is<Budget>(x => x.Amount == 2000 && x.YearMonth == "2017-02"));
+
+            Assert.IsTrue(wasCreated);
+        }
+
+
+        [TestMethod()]
+        public void CreateTest_when_exist_record_should_update_budget()
+        {
+            this._budgetService = new BudgetService(_budgetRepositoryStub);
+            var budgetFromDb = new Budget { Amount = 999, YearMonth = "2017-02" };
+            _budgetRepositoryStub.Read(Arg.Any<Func<Budget, bool>>()).ReturnsForAnyArgs(budgetFromDb);
+            var model = new BudgetAddViewModel { Amount = 2000, Month = "2017-02" };
+            var wasUpdated = false;
+            this._budgetService.Updated += (sender, args) => wasUpdated = true;
+            this._budgetService.Create(model);
+            _budgetRepositoryStub.Received().Save(Arg.Is<Budget>(x => x == budgetFromDb && x.Amount == 2000));
+            Assert.IsTrue(wasUpdated);
         }
     }
 }
