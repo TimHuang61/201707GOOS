19d477735c37498d0301e768dfb7fdce4746ecbc
Tim <tim.huang61@gmail.com>
2017-07-01 13:11:12 +0800

    重構測試案例 建立hook

---
+++ GOOS_SampleTests/Commons/Hooks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using FluentAutomation;
+using TechTalk.SpecFlow;
+
+namespace GOOS_SampleTests.Commons
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+        [BeforeScenario]
+        [Scope(Tag = "web")]
+        public void BeforeScenario()
+        {
+            SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+        }
+
+        //[AfterScenario]
+        //public void AfterScenario()
+        //{
+        //    //TODO: implement logic that has to run after executing each scenario
+        //}
+    }
+}
