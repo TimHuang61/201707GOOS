727ba364d530b2beeba3d7bf4a475dc033575dd0
Tim <tim.huang61@gmail.com>
2017-07-01 11:24:14 +0800

    add spec and page object

---
+++ GOOS_SampleTests/PageObjects/BudgetPageObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentAutomation;
+using GOOS_SampleTests.steps;
+
+namespace GOOS_SampleTests.PageObjects
+{
+    internal class BudgetCreatePage : PageObject<BudgetCreatePage>
+    {
+        public BudgetCreatePage(FluentTest test) : base(test)
+        {
+        }
+
+        public BudgetCreatePage Amount(object amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BudgetCreatePage Month(object yearMonth)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        internal BudgetCreatePage AddBudget()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BudgetCreatePage ShouldDisplay(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
